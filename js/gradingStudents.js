7bbe9027bdd36c01b000903115664f89f09f5135
armarce <ikkimasie@gmail.com>
2022-08-27 00:35:27 -0400

    Add gradingStudents

--- js/gradingStudents.js
+++ js/gradingStudents.js
@@ -14,17 +14,22 @@ function gradingStudents(grades){
 
             }else{
 
-                (5  - mod) < 3 ? newGrades.push()
+                (5  - mod) < 3 ? newGrades.push(grade + (5 - mod)) : newGrades.push(grade);
 
             }
 
         }else{
 
+            newGrades.push(grade);
 
         }
 
     }
 
+    return newGrades;
 
+}
 
-} 
+let grades = [75, 67, 40, 33];
+
+gradingStudents(grades);
